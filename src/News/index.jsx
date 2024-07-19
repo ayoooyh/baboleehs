76a159a4adcfad165d7fb87bbd45026e310e128c
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-07-19 23:21:42 +0900

    News/index.jsx 생성, App.jsx에 추가

---
+++ src/News/index.jsx
@@ -0,0 +1,16 @@
+export default function News() {
+  return (
+    <>
+      <div>News 1</div>
+      <div>News 2</div>
+      <div>News 3</div>
+      <div>News 4</div>
+      <div>News 5</div>
+      <div>News 6</div>
+      <div>News 7</div>
+      <div>News 8</div>
+      <div>News 9</div>
+      <div>News 10</div>
+    </>
+  );
+}
