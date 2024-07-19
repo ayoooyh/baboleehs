42997729fe78743a5343dff8726d38fa6befad99
lhs950204 <lhs950204@naver.com>
2024-07-19 23:11:58 +0900

    NewsPage 추가

---
+++ src/News/index.jsx
@@ -0,0 +1,16 @@
+export default function NewsPage() {
+  return (
+    <>
+      <div>News0</div>
+      <div>News1</div>
+      <div>News2</div>
+      <div>News3</div>
+      <div>News4</div>
+      <div>News5</div>
+      <div>News6</div>
+      <div>News7</div>
+      <div>News8</div>
+      <div>News9</div>
+    </>
+  );
+}
